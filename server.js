65b2834904ca13f2c82a92fc6be5e8f29844b6d1
MattV <mvpetersen1@gmail.com>
2021-05-09 15:02:12 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const recipes = [
+  "eggs",
+  "cake",
+  "cookies",
+  "bacon",
+  "fish",
+]
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.get("/api/random/camping/recipe", (req, res) => {
+const randomRecipeIndex = Math.floor(Math.random() * recipes.length);
+res.json(
+  recipes[randomRecipeIndex]
+)
+})
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
